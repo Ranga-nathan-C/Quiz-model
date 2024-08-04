dbb496771f6d1d0e907153375ae495a8f39c457b
Renganathanc <nathancranga4@gmail.com>
2024-08-04 16:24:17 +0530

    update2

--- script.js
+++ script.js
@@ -5,20 +5,29 @@ document.querySelectorAll('.option').forEach(option => {
         this.classList.add(isCorrect ? 'correct' : 'incorrect');
 
 
-        if(this.getAttribute('data-correct')==='false'){
+        if(!isCorrect){
             this.closest('.options').querySelectorAll('.option').forEach(opt1=>{
                 if(opt1.getAttribute('data-correct')==='true'){
-                    opt1.classList.add('correct')
+                    opt1.classList.add('correct'); 
+                    
                 }
-                
-            })      
+                opt1.style.pointerEvents = 'none'; 
+            
+            })  
+
         }   
+        else{
+            this.closest('.options').querySelectorAll('.option').forEach(opt2=>{
+                opt2.style.pointerEvents = 'none'; 
+            })
+
+        }
+
 
         const currentOptions = this.closest('.options').querySelector('.option');
         
         currentOptions.forEach(opt => {
             opt.classList.add(opt.getAttribute('data-correct') === 'true' ? 'correct' : 'incorrect');
-            opt.style.pointerEvents = 'none'; 
         });
     });
 });
